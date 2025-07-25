709a2ae4f637a0506d60785479464fca521a4ae9
marcopriests <prtmrc98@gmail.com>
2025-07-25 11:36:54 +0200

    [UPD] milestone 2 completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import useSwitch from "./customHooks/useSwitch"
+import useDate from "./customHooks/useDate";
 
 function App() {
   const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
   return (
     <>
       <div>
         <h1>Il valore è: {isOn ? 'ON' : 'OFF'}</h1>
         <button onClick={toggle}>Cambia stato</button>
+      </div>
+
+      <div>
+        <h1>Data e ora attuali:</h1>
+        <p>{currentDate.toLocaleString()}</p>
       </div>
     </>
   )
