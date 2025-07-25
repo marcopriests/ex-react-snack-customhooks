ccfae981956667e2f8788264151c72114867964e
marcopriests <prtmrc98@gmail.com>
2025-07-25 11:27:28 +0200

    [UPD] milestone 1 completata

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import useSwitch from "./customHooks/useSwitch"
+
+function App() {
+  const [isOn, toggle] = useSwitch();
+
+  return (
+    <>
+      <div>
+        <h1>Il valore è: {isOn ? 'ON' : 'OFF'}</h1>
+        <button onClick={toggle}>Cambia stato</button>
+      </div>
+    </>
+  )
+}
+
+export default App
